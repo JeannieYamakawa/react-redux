9fb4ca3eab514f39b6a8943e5228feb0aabed83b
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-28 23:52:13 -0600

    connects react and redux

---
+++ src/containers/book-list.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+    renderList(){
+        //this.props.book is brought in from the redux state of books property 
+        return this.props.books.map((book)=>{
+            return <li key={book.title} className='list-group-item'>{book.title}</li>
+        })
+    }
+    render(){
+        return (
+            <ul className='list-group col-sm-4'>
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+
+//this will take the application's state as the argument. it will instantly rerender with new changes.
+function mapStateToProps(state){
+    //whatever gets returned from here will show up as props inside of BookList above
+    return { books: state.books };
+
+}
+
+
+//this makes use of react-redux connect and says "take the mapStateToProps function and return a container." A container is a 'smart component that's aware of the state returned by Redux.
+export default connect(mapStateToProps)(BookList)
